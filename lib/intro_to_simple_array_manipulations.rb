b0372a6ee0c8cde653954fc91e71bf81649b393c
Brian Kao <bk1613@nyu.edu>
2019-09-02 19:51:01 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -7,38 +7,40 @@ def using_unshift(array, string)
 end
 
 def using_pop(array)
-
+  return
 end
 
 def pop_with_args(array)
-
+  return array.
 end
 
 def using_shift(array)
-
+  return array.
 end
 
 def shift_with_args(array)
-
+  return array.
 end
 
 def using_concat(array)
-
+  return array.
 end
 
 def using_insert(array)
-
+  return array.
 end
 
-def using_uniq(array, )
-
+def using_uniq(array)
+  return array.
 end
 
-def using_delete(array, )
-
+def using_delete(array, string)
+  return array.
 end
 
-
+def using_delete_at(array, integer)
+  return array.
+end
 
 
 
